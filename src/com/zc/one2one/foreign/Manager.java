866cf4a781c2d4dc5111055d8da890034b79e460
flyingzc <1262797000@qq.com>
2018-01-23 01:12:11 +0800

    Initial commit

---
+++ src/com/zc/one2one/foreign/Manager.java
@@ -0,0 +1,25 @@
+package com.zc.one2one.foreign;
+
+public class Manager {
+	private Integer mgrId;
+	private String mgrName;
+	private Department dept;//双向1对1
+	public Integer getMgrId() {
+		return mgrId;
+	}
+	public void setMgrId(Integer mgrId) {
+		this.mgrId = mgrId;
+	}
+	public String getMgrName() {
+		return mgrName;
+	}
+	public void setMgrName(String mgrName) {
+		this.mgrName = mgrName;
+	}
+	public Department getDept() {
+		return dept;
+	}
+	public void setDept(Department dept) {
+		this.dept = dept;
+	}
+}
